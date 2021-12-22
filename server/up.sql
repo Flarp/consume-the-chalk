7a004e6edbd64035d7fb8505f5c1159c938bba73
Flarp <flurpdadurp@gmail.com>
2021-12-22 14:10:41 -0500

    use scraper interpolation in SQL

--- server/up.sql
+++ server/up.sql
@@ -1,5 +1,20 @@
-CREATE DATABASE IF NOT EXISTS `chalk`;
+CREATE DATABASE IF NOT EXISTS chalk;
+USE chalk;
 
-CREATE TABLE IF NOT EXISTS `chalkboards` (
-  id int NOT NULL,
-)
+CREATE TABLE IF NOT EXISTS chalkboards (
+  id INT NOT NULL PRIMARY KEY,
+  building ENUM(@BUILDINGS@), -- BUILDINGS will be replaced by scraping program
+  room CHAR(4) NOT NULL,
+  panels TINYINT NOT NULL,
+  width ENUM("normal", "wide", "extra wide") NOT NULL,
+  color ENUM("black", "green", "brown") NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS occupancies (
+  id INT NOT NULL PRIMARY KEY,
+  building ENUM(@BUILDINGS@),
+  room CHAR(4) NOT NULL,
+  days TINYINT NOT NULL,
+  start_time TIME NOT NULL,
+  end_time TIME NOT NULL,
+);
