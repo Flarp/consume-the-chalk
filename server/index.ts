694895df6f1e0e8742302094a07596b3c73399c7
Flarp <flurpdadurp@gmail.com>
2021-12-22 23:04:47 -0500

    switch from mysql to postgres

--- server/index.ts
+++ server/index.ts
@@ -1,14 +1,26 @@
 import express, { Request, Response } from "express"
+import { Pool } from "pg"
+//import jsrsasign from "jsrsasign"
 import path from "path"
 
 const app = express()
+
+const pool = new Pool({
+  user: "postgres",
+  password: "",
+  database: "postgres",
+  port: 5432,
+  //host: "localhost"
+})
 
 app.use(express.static(path.join(__dirname, "web")))
 //console.log(path.join(__dirname, "../../client/dist"))
 
 // most normal usage
 app.get("/chalkboards", async (req: Request, res: Response) => {
-  console.log(req)
+  const {rows} = await pool.query("SELECT * FROM occupancies LIMIT 10;")
+  console.log(rows)
+  //console.log(req)
 })
 
 // admin only
